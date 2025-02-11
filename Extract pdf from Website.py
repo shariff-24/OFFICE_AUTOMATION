456406f014562d6e7dc7a43829914b2ee9eaa2e4
shariff-24 <xtoolindia1@gmail.com>
2025-02-11 16:44:09 +0530

    Update Extract pdf from Website.py

--- Extract pdf from Website.py
+++ Extract pdf from Website.py
@@ -85,13 +85,13 @@ def extract_url_pdf(input_url,folder_path=os.getcwd()):
 # In[5]:
 
 
-extract_url_pdf(input_url="https://www.icai.org/post.html?post_id=17843")
+extract_url_pdf(input_url="https://web.classplusapp.com/store/course/581092/29111372?section=content")
 
 
 # In[6]:
 
 
-extract_url_pdf(input_url="https://cbic-gst.gov.in/central-tax-notifications.html")
+extract_url_pdf(input_url="https://web.classplusapp.com/store/course/581092/29111372?section=content")
 
 # https://cbic-gst.gov.in/central-tax-notifications.html
 
